57edb672a53cc6b9fc8646d7b099f106ff808bd3
reuben-raj <reuben.raj90@gmail.com>
2024-08-19 23:12:16 +0800

    initial commit

---
+++ src/main/java/com/mbb/api/sales_tracker/SalesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mbb.api.sales_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTrackerApplication.class, args);
+	}
+
+}
